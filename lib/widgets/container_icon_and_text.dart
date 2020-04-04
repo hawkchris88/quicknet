0cf1d9d195f7f516254c5c536be36c5480e58d6a
iMarK <marksonline247@gmail.com>
2020-04-04 09:13:29 +0000

    Create container_icon_and_text.dart

---
+++ lib/widgets/container_icon_and_text.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ContainerIconAndText extends StatelessWidget {
+  final IconData icon;
+  final String iconText;
+  final Function onPressed;
+
+  ContainerIconAndText(
+      {@required this.icon, @required this.iconText, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Material(
+              shape: CircleBorder(
+                side: BorderSide(
+                  color: Color(0xff348977),
+                  width: 3.0,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  icon,
+                  size: 85.0,
+                  color: Color(0xff348977),
+                ),
+              ),
+            ),
+            Text(
+              iconText,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
